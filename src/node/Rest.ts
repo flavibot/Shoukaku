677ba74c8b351997a38d822b3a5881d98ec43069
flav-code <flavcraft28@gmail.com>
2024-08-13 10:28:35 +0000

    chore: update error handling in Rest class

--- src/node/Rest.ts
+++ src/node/Rest.ts
@@ -383,7 +383,7 @@ export class Rest {
 		const request = await fetch(url.toString(), finalFetchOptions)
 			.finally(() => clearTimeout(timeout))
 			.catch((err) => {
-				throw new Error(`[NODE-REST] (${this.node.name}) ${err.message || err || 'Unknown error'}`);
+				throw new Error(`[NODE-REST] (${this.node.name}) ${err?.message || err || 'Unknown error'}`);
 			});
 
 		if (!request.ok) {
@@ -429,7 +429,7 @@ export class RestError extends Error {
 		this.status = status;
 		this.error = error;
 		this.trace = trace;
-		this.message = `[NODE-REST] (${nodeName}) ${message}`;
+		this.message = `(${nodeName}) ${message}`;
 		this.path = path;
 		Object.setPrototypeOf(this, new.target.prototype);
 	}
