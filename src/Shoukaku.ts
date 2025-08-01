7024b95c3295212ad2629a28dd0f4f10f7e8bae3
flav-code <flavcraft28@gmail.com>
2025-08-01 16:36:24 +0000

    feat: add 'rest' event emission and enhance FetchOptions interface in Rest class

--- src/Shoukaku.ts
+++ src/Shoukaku.ts
@@ -1,10 +1,10 @@
 import { ShoukakuDefaults, VoiceState } from './Constants';
-import { Node } from './node/Node';
 import { Connector } from './connectors/Connector';
-import { Constructor, mergeDefault, TypedEventEmitter } from './Utils';
-import { Player } from './guild/Player';
-import { Rest } from './node/Rest';
 import { Connection } from './guild/Connection';
+import { Player } from './guild/Player';
+import { Node, NodeEvents } from './node/Node';
+import type { FetchOptions, Rest } from './node/Rest';
+import { Constructor, mergeDefault, TypedEventEmitter } from './Utils';
 
 export interface Structures {
 	/**
@@ -134,7 +134,24 @@ export type ShoukakuEvents = {
      * @eventProperty
      */
 	'raw': [name: string, json: unknown];
+	/**
+	 * Emitted when a rest request is made to Lavalink
+	 * @eventProperty
+	 */
+	'rest': [name: string, data: RestEventData];
 };
+
+/**
+ * Rest event data
+ */
+export interface RestEventData {
+    url: string;
+    options: FetchOptions;
+    status: number;
+    ok: boolean;
+    latency: number;
+    retries: number;
+}
 
 /**
  * Main Shoukaku class
@@ -215,6 +232,7 @@ export class Shoukaku extends TypedEventEmitter<ShoukakuEvents> {
 		node.on('close', (...args) => this.emit('close', node.name, ...args));
 		node.on('ready', (...args) => this.emit('ready', node.name, ...args));
 		node.on('raw', (...args) => this.emit('raw', node.name, ...args));
+		node.on('rest', (...args) => this.emit('rest', node.name, ...args));
 		node.once('disconnect', () => this.nodes.delete(node.name));
 		node.connect().catch((error) => this.emit('error', node.name, error as Error));
 		this.nodes.set(node.name, node);
